16ab111ec29501f5da1273b78cf7edbc1aede3d0
aya chouate <chouateaya@gmail.com>
2023-10-24 15:52:06 +0100

    Ajout CustomerManager

---
+++ src/main/java/com/chouate/tpcustomerchouate/service/CustomerManager.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chouate.tpcustomerchouate.service;
+
+import com.chouate.tpcustomerchouate.entity.Customer;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Classe pour gérer la persistance des Customers.
+ * <p>
+ * Cette classe fournit des méthodes pour effectuer des opérations CRUD sur des
+ * instances de Customer.
+ * </p>
+ */
+@RequestScoped
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    @Transactional
+    public void persist(Customer customer) {
+        em.persist(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
+
+    }
+
+    @Transactional
+    public Customer update(Customer customer) {
+        return em.merge(customer);
+    }
+}
