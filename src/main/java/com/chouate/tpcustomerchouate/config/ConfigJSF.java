ac2c58c6288237905defc612a4f52d6ef7cd560c
aya chouate <chouateaya@gmail.com>
2023-10-29 16:00:25 +0100

    Ajout customerDetails.xhtml

---
+++ src/main/java/com/chouate/tpcustomerchouate/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chouate.tpcustomerchouate.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author tech
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
